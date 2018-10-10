0ef70d9a862ab2bf0085c8229f13b69eef65809e
autodocs <autodocs>
2018-10-10 14:26:31 +0000

    build based on b041d9c

--- latest/search_index.js
+++ latest/search_index.js
@@ -9,19 +9,35 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "index.html#ParseTrees.parse_tree-Tuple{Any}",
+    "location": "index.html#ParseTrees.annotate",
     "page": "Home",
-    "title": "ParseTrees.parse_tree",
-    "category": "method",
-    "text": "parse_tree(file, format = \"enhanced-plus-plus-dependencies\")\n\nProcess the results of the coreNLP parse annotator from an XML file.\n\nFor each sentence, will return meta data about each word as well as a tree of how the words are connected.\n\nYou can run coreNLP through the command line. A minimal command is java -cp \"*\" edu.stanford.nlp.pipeline.StanfordCoreNLP -annotators tokenize,ssplit,parse -fileList FILE_LIST from the folder where you unzipped coreNLP. The resulting files will be in the format FILE.xml, each of which you can parse with parse_tree.\n\njulia> using ParseTrees\n\njulia> result = parse_tree(\"hammurabi.txt.xml\");\n\njulia> result[31].meta\n17-element Array{NamedTuple{(:relationship, :text),Tuple{String,String}},1}:\n (relationship = \"mark\", text = \"If\")\n (relationship = \"det\", text = \"any\")\n (relationship = \"nsubj\", text = \"one\")\n (relationship = \"advcl:if\", text = \"steal\")\n (relationship = \"det\", text = \"the\")\n (relationship = \"amod\", text = \"minor\")\n (relationship = \"dobj\", text = \"son\")\n (relationship = \"case\", text = \"of\")\n (relationship = \"nmod:of\", text = \"another\")\n (relationship = \"punct\", text = \",\")\n (relationship = \"nsubjpass\", text = \"he\")\n (relationship = \"aux\", text = \"shall\")\n (relationship = \"auxpass\", text = \"be\")\n (relationship = \"root\", text = \"put\")\n (relationship = \"case\", text = \"to\")\n (relationship = \"nmod:to\", text = \"death\")\n (relationship = \"punct\", text = \".\")\n\njulia> result[31].tree\n{16, 15} directed simple Int64 graph\n\n\n\n\n\n"
+    "title": "ParseTrees.annotate",
+    "category": "function",
+    "text": "annotate(file, annotators = (\"tokenize\", \"ssplit\", \"parse\"), options = ``)\n\nReturn a command line call to coreNLP to be run.\n\nMust be run with the working directory set to the location where you have unzipped coreNLP. Will take a few minutes to complete.\n\njulia> using ParseTrees\n\njulia> cd(\"C:/Users/hp/stanford-corenlp-full-2018-10-05\")\n\njulia> annotate(\"hammurabi.txt\")\n`java -cp \'*\' edu.stanford.nlp.pipeline.StanfordCoreNLP -annotators tokenize,ssplit,parse -file hammurabi.txt`\n\n\n\n\n\n"
 },
 
 {
-    "location": "index.html#ParseTrees.separate_clauses-Tuple{Any,Any}",
+    "location": "index.html#ParseTrees.clauses-Tuple{Any,Any,Any}",
     "page": "Home",
-    "title": "ParseTrees.separate_clauses",
+    "title": "ParseTrees.clauses",
     "category": "method",
-    "text": "separate_clauses(sentence, clause_types)\n\nPull out root level clauses from a parsed sentence that are one of the clause_types.\n\njulia> using ParseTrees\n\njulia> sentence = parse_tree(\"hammurabi.txt.xml\")[31];\n\njulia> result = separate_clauses(sentence, (\"advcl:if\", \"nsubjpass\", \"aux\"));\n\njulia> result.clauses[1]\n(clause_type = \"advcl:if\", text = \"If any one steal the minor son of another\")\n\njulia> result.clauses[2]\n(clause_type = \"nsubjpass\", text = \"he\")\n\njulia> result.clauses[3]\n(clause_type = \"aux\", text = \"shall\")\n\njulia> result.rest\n\", be put to death\"\n\n\n\n\n\n"
+    "text": "clauses(meta, tree, clause_types)\n\nPull out root level clauses from a parsed sentence that are one of the clause_types.\n\njulia> using ParseTrees\n\njulia> depencies_31 = collect(sentences(\"hammurabi.txt.xml\"))[31] |> dependencies;\n\njulia> clauses_31 = clauses(depencies_31.meta, depencies_31.tree, (\"advcl\", \"nsubjpass\", \"aux\"));\n\njulia> clauses_31.clauses[1]\n(clause_type = \"advcl\", text = \"If any one steal the minor son of another\")\n\njulia> clauses_31.clauses[2]\n(clause_type = \"nsubjpass\", text = \"he\")\n\njulia> clauses_31.clauses[3]\n(clause_type = \"aux\", text = \"shall\")\n\njulia> clauses_31.rest\n\", be put to death\"\n\n\n\n\n\n"
+},
+
+{
+    "location": "index.html#ParseTrees.dependencies",
+    "page": "Home",
+    "title": "ParseTrees.dependencies",
+    "category": "function",
+    "text": "dependencies(sentence, format = \"basic-dependencies\")\n\nReturn meta data about each token as well as a tree of how the tokens are connected.\n\nInput one of the sentences of a coreNLP run containing at least the tokenize, ssplit, and parse annotators.\n\njulia> using ParseTrees\n\njulia> depencies_31 = collect(sentences(\"hammurabi.txt.xml\"))[31] |> dependencies;\n\njulia> depencies_31.meta\n17-element Array{NamedTuple{(:relationship, :text),Tuple{String,String}},1}:\n (relationship = \"mark\", text = \"If\")\n (relationship = \"det\", text = \"any\")\n (relationship = \"nsubj\", text = \"one\")\n (relationship = \"advcl\", text = \"steal\")\n (relationship = \"det\", text = \"the\")\n (relationship = \"amod\", text = \"minor\")\n (relationship = \"dobj\", text = \"son\")\n (relationship = \"case\", text = \"of\")\n (relationship = \"nmod\", text = \"another\")\n (relationship = \"punct\", text = \",\")\n (relationship = \"nsubjpass\", text = \"he\")\n (relationship = \"aux\", text = \"shall\")\n (relationship = \"auxpass\", text = \"be\")\n (relationship = \"root\", text = \"put\")\n (relationship = \"case\", text = \"to\")\n (relationship = \"nmod\", text = \"death\")\n (relationship = \"punct\", text = \".\")\n\njulia> depencies_31.tree\n{16, 15} directed simple Int64 graph\n\n\n\n\n\n"
+},
+
+{
+    "location": "index.html#ParseTrees.sentences-Tuple{Any}",
+    "page": "Home",
+    "title": "ParseTrees.sentences",
+    "category": "method",
+    "text": "sentences(file)\n\nReturn an iterator over sentences.\n\nInput the XML results a run of coreNLP including at least the tokenize and ssplit annotators.\n\njulia> using ParseTrees\n\njulia> result = sentences(\"hammurabi.txt.xml\");\n\njulia> typeof(first(result))\nLightXML.XMLElement\n\n\n\n\n\n"
 },
 
 {
