c658a14f32c3a06c6f6a94d0b13c72022d49e722
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-01-05 01:26:00 +0000

    build based on fbef226

--- dev/search_index.js
+++ dev/search_index.js
@@ -17,11 +17,11 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "#ParseTrees.institutional_grammar",
+    "location": "#ParseTrees.dictionary",
     "page": "ParseTrees.jl",
-    "title": "ParseTrees.institutional_grammar",
+    "title": "ParseTrees.dictionary",
     "category": "constant",
-    "text": "institutional_grammar\n\nA clause dict based on Elinor Ostrom\'s institutional grammar. Of the forty universal dependencies, many are labelled as :not_applicable, meaning they will not occur in the root. Several are labelled :not_sure, meaning it could or could not be one of the 6 components of institutional grammar. Then there are the ABDICO components, less Or else, which can\'t be determined grammatically, and aIm, which will end up as rest. Rules inside rules are marked for recursion, and superfluous clauses are marked for removel.\n\n\n\n\n\n"
+    "text": "dictionary\n\nA clause dict based on Elinor Ostrom\'s institutional grammar. Of the forty universal dependencies, many are labelled as :not_applicable, meaning they will not occur in the root. Several are labelled :not_sure, meaning it could or could not be one of the 6 components of institutional grammar. Then there are the ABDICO components, less Or else, which can\'t be determined grammatically, and aIm, which will end up as rest. Rules inside rules are marked for recursion, and superfluous clauses are marked for removel.\n\n\n\n\n\n"
 },
 
 {
@@ -37,7 +37,7 @@ var documenterSearchIndex = {"docs": [
     "page": "ParseTrees.jl",
     "title": "ParseTrees.rules",
     "category": "method",
-    "text": "rules(file; dictionary = institutional_grammar, deontic = deontic, rest = :rest)\n\nSplit the sentences of a file into groups of clauses based on dictionary, starting at the root. Clause dict should be a map from Universal Dependencies (v1) to clause categories. Rules will be identified if they contain both a clause in the :Deontic category and also matching the deontic pattern. There are three additional reserved clause categories, :recur (which will look for a rule inside the rule), :remove, (which will ignore the clause), and rest (which will gobble up any uncategorized root-level clauses).\n\njulia> using ParseTrees\n\njulia> result = rules(\"hammurabi.txt.xml\");\n\njulia> result[237]\n5-element Array{Pair{Symbol,String},1}:\n :Condition => \"If any one hire a cart alone\"\n :Attribute => \"he\"\n   :Deontic => \"shall\"\n    :oBject => \"forty ka of corn per day\"\n       :aIm => \"pay\"\n\n\n\n\n\n"
+    "text": "rules(file; dictionary = dictionary, deontic = deontic, rest = :aIm)\n\nSplit the sentences of a file into groups of clauses based on dictionary, starting at the root. Clause dict should be a map from Universal Dependencies (v1) to clause categories. Rules will be identified if they contain both a clause in the :Deontic category and also matching the deontic pattern. There are three additional reserved clause categories, :recur (which will look for a rule inside the rule), :remove, (which will ignore the clause), and rest (which will gobble up any uncategorized root-level clauses).\n\njulia> using ParseTrees\n\njulia> result = rules(\"hammurabi.txt.xml\");\n\njulia> result[237]\n5-element Array{Pair{Symbol,String},1}:\n :Condition => \"If any one hire a cart alone\"\n :Attribute => \"he\"\n   :Deontic => \"shall\"\n    :oBject => \"forty ka of corn per day\"\n       :aIm => \"pay\"\n\n\n\n\n\n"
 },
 
 {
